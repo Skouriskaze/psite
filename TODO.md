cb0825422d748527b706810f015264baff7eaca2
Skouri <skouriskaze>
2017-09-12 02:55:13 +0000

    Changed template

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO LIST
+*Minify the css file
+*Clean up resources (imags)
+*Replace images
--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var http = require('http');
-var dt = require('./datemodule');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    // req.url
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently" + dt.myDateTime() + '\n');
-    res.write('checking');
-    res.end();
-}).listen(3000)
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.get('/product', function(req, res) {
+    res.render('pages/product');
+});
+
+app.get('/template', function(req, res) {
+    res.render('pages/index');
+});
+
+
+app.listen(3000, function() {
+    console.log('Leggo port 3000');
+});
